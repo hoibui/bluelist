1f2062fa7b37c4c0f07347a3d57f6b2f5d1b6c45
hoibui <athoibuiqt@gmail.com>
2016-04-20 14:23:01 +0700

    Vi Du

---
+++ DANHSACHMONAN/Form1.Designer.cs
@@ -0,0 +1,231 @@
+﻿namespace DANHSACHMONAN
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.cboChonMien = new System.Windows.Forms.ComboBox();
+            this.lsbDanhSach = new System.Windows.Forms.ListBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtHinh = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtTen = new System.Windows.Forms.TextBox();
+            this.cboMien = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtMa = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(58, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Chon Mien";
+            // 
+            // cboChonMien
+            // 
+            this.cboChonMien.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cboChonMien.FormattingEnabled = true;
+            this.cboChonMien.Location = new System.Drawing.Point(72, 4);
+            this.cboChonMien.Name = "cboChonMien";
+            this.cboChonMien.Size = new System.Drawing.Size(121, 21);
+            this.cboChonMien.TabIndex = 1;
+            this.cboChonMien.SelectedIndexChanged += new System.EventHandler(this.cboChonMien_SelectedIndexChanged);
+            // 
+            // lsbDanhSach
+            // 
+            this.lsbDanhSach.FormattingEnabled = true;
+            this.lsbDanhSach.Location = new System.Drawing.Point(1, 67);
+            this.lsbDanhSach.Name = "lsbDanhSach";
+            this.lsbDanhSach.Size = new System.Drawing.Size(292, 407);
+            this.lsbDanhSach.TabIndex = 2;
+            this.lsbDanhSach.SelectedIndexChanged += new System.EventHandler(this.lsbDanhSach_SelectedIndexChanged);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(299, 67);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(255, 174);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.txtHinh);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.txtTen);
+            this.groupBox1.Controls.Add(this.cboMien);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.txtMa);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Location = new System.Drawing.Point(299, 276);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(255, 198);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Thong Tin Mon An";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(147, 168);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Xoa";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(19, 168);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Cap Nhat";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // txtHinh
+            // 
+            this.txtHinh.Location = new System.Drawing.Point(77, 91);
+            this.txtHinh.Name = "txtHinh";
+            this.txtHinh.Size = new System.Drawing.Size(129, 20);
+            this.txtHinh.TabIndex = 1;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(6, 94);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(67, 13);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "Hinh mon an";
+            // 
+            // txtTen
+            // 
+            this.txtTen.Location = new System.Drawing.Point(77, 61);
+            this.txtTen.Name = "txtTen";
+            this.txtTen.Size = new System.Drawing.Size(129, 20);
+            this.txtTen.TabIndex = 1;
+            // 
+            // cboMien
+            // 
+            this.cboMien.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cboMien.FormattingEnabled = true;
+            this.cboMien.Location = new System.Drawing.Point(76, 122);
+            this.cboMien.Name = "cboMien";
+            this.cboMien.Size = new System.Drawing.Size(130, 21);
+            this.cboMien.TabIndex = 1;
+            this.cboMien.SelectedIndexChanged += new System.EventHandler(this.cboChonMien_SelectedIndexChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 64);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(64, 13);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Ten mon an";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(8, 129);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(64, 13);
+            this.label5.TabIndex = 0;
+            this.label5.Text = "Thuoc Mien";
+            // 
+            // txtMa
+            // 
+            this.txtMa.Location = new System.Drawing.Point(77, 32);
+            this.txtMa.Name = "txtMa";
+            this.txtMa.Size = new System.Drawing.Size(129, 20);
+            this.txtMa.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 35);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(60, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Ma mon an";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(566, 484);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.lsbDanhSach);
+            this.Controls.Add(this.cboChonMien);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cboChonMien;
+        private System.Windows.Forms.ListBox lsbDanhSach;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox txtHinh;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtTen;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtMa;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox cboMien;
+    }
+}
+
---
+++ DANHSACHMONAN/Form1.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Collections;
+
+namespace DANHSACHMONAN
+{
+    public partial class Form1 : Form
+    {
+        class MonAn
+        {
+
+            public string Ten { get; set; }
+
+            public string Hinh { get; set; }
+
+            public string Mien { get; set; }
+        }
+        const string BAC = "BẮC";
+        const string TRUNG = "MIENTRUNG";
+        const string NAM = "MIENNAM"; 
+        const string TATCA = "TẤT CẢ";
+        SortedList<String, MonAn> monan;
+        public Form1()
+        {
+            InitializeComponent();
+            DanhSachMon();
+        }
+
+        private void DanhSachMon()
+        {
+            monan = new SortedList<string, MonAn>
+            {
+            { "BC001", new MonAn{Ten = "Bánh Chưng",Hinh="banh_chung.jpg", Mien = BAC }},
+            
+         
+           
+        };
+        //    monan = new SortedList<string, MonAn>
+        //    {
+            
+        //    { "BL001", new MonAn{Ten = "Bánh Bột Lọc",Hinh="banh-bot-loc.jpg", Mien = TRUNG }},
+         
+           
+        //};
+
+        }
+
+        private void cboChonMien_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            cboChonMien.Items.Add(BAC);
+            cboChonMien.Items.Add(TRUNG);
+            cboChonMien.Items.Add(NAM);
+            cboChonMien.Items.Add(TATCA);
+            cboMien.Items.Add(BAC);
+            cboMien.Items.Add(TRUNG);
+            cboMien.Items.Add(NAM);
+            cboMien.Items.Add(TATCA);
+            cboMien.SelectedIndex = cboChonMien.SelectedIndex = 0;
+
+            XuatDanhSach();
+            //lsbDanhSach.SelectedIndex = 0;
+
+        }
+
+        private void XuatDanhSach()
+        {
+            lsbDanhSach.Items.Clear();
+            foreach(KeyValuePair<string, MonAn>pair in monan)
+            {
+                if (cboChonMien.Text == TATCA||pair.Value.Mien==cboChonMien.Text)
+                    lsbDanhSach.Items.Add(pair.Key + " - " + pair.Value.Ten);
+            }
+        }
+
+        private void lsbDanhSach_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (lsbDanhSach.SelectedIndex < 0) return;
+            int index = lsbDanhSach.Text.IndexOf('-');
+            string key = lsbDanhSach.Text.Substring(0, index).Trim();
+            MonAn mon;
+            bool KetQua = monan.TryGetValue(key, out mon);
+            if(!KetQua)
+            {
+                MessageBox.Show("Lỗi");
+                return;
+            }
+
+            txtMa.Text = key;
+            txtTen.Text = mon.Ten;
+            txtHinh.Text = mon.Hinh;
+            pictureBox1.ImageLocation = @"Image\" + mon.Hinh;
+            cboMien.Text = cboChonMien.Text;
+        }
+    }
+}
